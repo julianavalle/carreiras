afd20bb6ad775fe77c4123df8d94ba599f945fc1
Juliana Valle <juju.pvg@gmail.com>
2021-10-04 23:28:50 -0300

    Creating routes

--- material-agora/src/App.js
+++ material-agora/src/App.js
@@ -1,18 +1,26 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Heros from './pages/Heros';
+import Details from './pages/Details';
+import Groups from './pages/Groups';
 
 function App() {
   return (
     <Switch>
-      <Route path='/heros'>
+      <Route path='/' exact>
+        <Redirect to='heros' />
+      </Route>
 
+      <Route path='/heros' exact>
+        <Heros />
       </Route>
 
       <Route path='/heros/:heroId'>
-        
+        <Details />
       </Route>
 
       <Route path='/groups'>
-        
+        <Groups />
       </Route>
     </Switch>
   );
